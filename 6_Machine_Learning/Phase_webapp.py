d3e8717d1e9a961a711f039d9423e04d625dc983
subediupadesh <upadesh.subedi@gmail.com>
2024-09-12 01:04:09 +0200

    model_uploaded

--- 6_Machine_Learning/Phase_webapp.py
+++ 6_Machine_Learning/Phase_webapp.py
@@ -324,7 +324,7 @@ elif option == 1:
     contour_levels = [450, 700,  1337]
     label_colors = ['k', 'k', 'aqua']
     contour = ax3.contour(X_test_array[0][0], levels=contour_levels, colors='crimson', linewidths=3, linestyles='dashdot')
-    labels = plt.clabel(contour, inline=True, fontsize=24, fmt='%1.0f K')
+    labels = plt.clabel(contour, inline=True, fontsize=20, fmt='%1.0f K')
     for label, color in zip(labels, label_colors): label.set_fontweight('bold');label.set_color(color) 
 
     cmap = plt.get_cmap('RdYlGn_r')
